c39702fe1919a83b81cd407d285c8070caeced09
jaisic13 <jaisichawra@gmail.com>
2025-06-22 15:57:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+
+import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+
+chat_history_ids = None
+
+def respond(message):
+    global chat_history_ids
+    new_input_ids = tokenizer.encode(message + tokenizer.eos_token, return_tensors='pt')
+    bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1) if chat_history_ids is not None else new_input_ids
+    chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    return response
+
+gr.Interface(fn=respond, inputs="text", outputs="text", title="Morphius AI Chatbot").launch()
